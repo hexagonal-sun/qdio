d56d7cdba09ed951b2622b3fdfe6afb42baf18c6
Matthew Leach <matthew@mattleach.net>
2018-03-16 19:56:18 +0000

    carts: controlButton: implement
    Implement a basic control button with it's own colours, etc.

--- src/carts/controlButton.cpp
+++ src/carts/controlButton.cpp
@@ -1,6 +1,33 @@
+#include <QPainter>
+
 #include "controlButton.h"
 
-ControlButton::ControlButton()
+ControlButton::ControlButton(QWidget *parent)
+    : QPushButton(parent)
 {
+    QFont widgetFont = font();
 
+    widgetFont.setBold(true);
+    widgetFont.setPointSize(widgetFont.pointSize() * 1.5);
+
+    setFont(widgetFont);
+    setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Minimum);
+}
+
+void ControlButton::setText(const QString &text)
+{
+    QPushButton::setText("\n" + text + "\n");
+}
+
+void ControlButton::paintEvent(QPaintEvent *pe)
+{
+    QPainter painter(this);
+    QPen pen(Qt::yellow, 3);
+
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setPen(pen);
+    painter.setBrush(Qt::red);
+    painter.drawRect(pe->rect());
+
+    painter.drawText(pe->rect(), Qt::AlignCenter, text());
 }
--- src/carts/controlButton.h
+++ src/carts/controlButton.h
@@ -1,11 +1,16 @@
 #ifndef CONTROLBUTTON_H
 #define CONTROLBUTTON_H
 
+#include <QPushButton>
+#include <QPaintEvent>
 
 class ControlButton : public QPushButton
 {
 public:
-    ControlButton();
+    ControlButton(QWidget *parent);
+    void setText(const QString &text);
+private:
+    void paintEvent(QPaintEvent *pe);
 };
 
 #endif // CONTROLBUTTON_H
