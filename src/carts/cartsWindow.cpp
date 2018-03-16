72b6d8c358e85bc11f6e61f207bcdfffad24e639
Matthew Leach <matthew@mattleach.net>
2018-03-16 20:09:41 +0000

    carts: cartsWindow: {logout,stopAll}Button: new
    Add new control buttons to the carts window for stopping all carts and
    logging out of the current session.

--- src/carts/cartsWindow.cpp
+++ src/carts/cartsWindow.cpp
@@ -1,17 +1,31 @@
 #include <QVBoxLayout>
 
 #include "cartsWindow.h"
+#include "controlButton.h"
 #include "cartWall.h"
 
 CartsWindow::CartsWindow(QWidget *parent) :
     QMainWindow(parent)
 {
     QVBoxLayout *layout = new QVBoxLayout(this);
+    QHBoxLayout *controlButtonLayout = new QHBoxLayout(this);
     CartWall *topCartWall = new CartWall(this, 0);
     CartWall *bottomCartWall = new CartWall(this, 1);
     QWidget *mainWidget = new QWidget(this);
+    ControlButton *logoutButton = new ControlButton(this);
+    ControlButton *stopAllButton = new ControlButton(this);
+
+    logoutButton->setText("Logout");
+    stopAllButton->setText("Stop All");
+
+    controlButtonLayout->addStretch();
+    controlButtonLayout->addWidget(logoutButton);
+    controlButtonLayout->addSpacing(35);
+    controlButtonLayout->addWidget(stopAllButton);
+    controlButtonLayout->addStretch();
 
     layout->addWidget(topCartWall);
+    layout->addLayout(controlButtonLayout);
     layout->addWidget(bottomCartWall);
 
     mainWidget->setLayout(layout);
