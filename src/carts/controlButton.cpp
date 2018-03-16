768118c552bcfa547f394f7daed7cba355572a90
Matthew Leach <matthew@mattleach.net>
2018-03-16 17:37:20 +0000

    carts: controlButton: new
    Add a new subclass of QPushButton that represents a control button.

---
+++ src/carts/controlButton.cpp
@@ -0,0 +1,6 @@
+#include "controlButton.h"
+
+ControlButton::ControlButton()
+{
+
+}
---
+++ src/carts/controlButton.h
@@ -0,0 +1,11 @@
+#ifndef CONTROLBUTTON_H
+#define CONTROLBUTTON_H
+
+
+class ControlButton : public QPushButton
+{
+public:
+    ControlButton();
+};
+
+#endif // CONTROLBUTTON_H
