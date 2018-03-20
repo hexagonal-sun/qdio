50d47785ca0af2b86c3fc48a03bfeb19602e808e
Matthew Leach <matthew@mattleach.net>
2018-03-20 20:13:33 +0000

    carts: cartWall: display cart page buttons
    Display the cart page buttons in the cartwall.

--- src/carts/cartWall.cpp
+++ src/carts/cartWall.cpp
@@ -1,4 +1,10 @@
 #include "cartWall.h"
+#include "cartPageButton.h"
+
+auto constexpr NO_CART_ROWS = 4;
+auto constexpr NO_CART_COLS = 5;
+auto constexpr LHS_PAGE_COL = 0;
+auto constexpr RHS_PAGE_COL = NO_CART_COLS + 1;
 
 CartWall::CartWall(QWidget *parent, int cartWallId)
     : QWidget(parent),
@@ -6,9 +12,10 @@ CartWall::CartWall(QWidget *parent, int cartWallId)
 {
     gridLayout_= new QGridLayout(this);
 
-    for (uint32_t x = 0; x < 4; x++) {
-        std::vector<CartButton *>buttonRow;
-        for (uint32_t y = 0; y < 4; y++) {
+    for (auto x = 0; x < NO_CART_COLS; x++) {
+        std::vector<CartButton *>buttonCol;
+
+        for (auto y = 0; y < NO_CART_ROWS; y++) {
             QString title = "";
 
             auto btn = new CartButton(this, title,
@@ -16,12 +23,27 @@ CartWall::CartWall(QWidget *parent, int cartWallId)
 
             btn->setEnabled(false);
 
-            buttonRow.push_back(btn);
+            buttonCol.push_back(btn);
 
-            gridLayout_->addWidget(btn, x, y);
+            gridLayout_->addWidget(btn, y, x + 1);
         }
-        buttons.push_back(buttonRow);
+
+        buttons.push_back(buttonCol);
     }
+
+    for (auto page = 0; page < NO_CART_ROWS; page++) {
+        auto lhsPageButton = new CartPageButton(this, page);
+        auto rhsPageButton = new CartPageButton(this, page + NO_CART_ROWS);
+
+        gridLayout_->addWidget(lhsPageButton, page, LHS_PAGE_COL);
+        gridLayout_->addWidget(rhsPageButton, page, RHS_PAGE_COL);
+    }
+
+    gridLayout_->setColumnStretch(LHS_PAGE_COL, 1);
+    gridLayout_->setColumnStretch(RHS_PAGE_COL, 1);
+
+    for (auto i = 0; i < NO_CART_COLS; i++)
+        gridLayout_->setColumnStretch(i + 1, 3);
 
     setLayout(gridLayout_);
 }
