1f33bf295a8e1ee8a713bff5dfb20b28463c0630
Matthew Leach <matthew@mattleach.net>
2018-03-23 20:18:53 +0000

    sessions: settingsDilaog: new
    Add a new dialog for setting and viewing qdio playout settings.

---
+++ src/sessions/settingsDialog.cpp
@@ -0,0 +1,14 @@
+#include "settingsDialog.h"
+#include "ui_settingsDialog.h"
+
+SettingsDialog::SettingsDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::SettingsDialog)
+{
+    ui->setupUi(this);
+}
+
+SettingsDialog::~SettingsDialog()
+{
+    delete ui;
+}
