fce87c13d5860c684e8707ba5f9557661bcbd466
Matthew Leach <matthew@mattleach.net>
2018-03-30 18:49:17 +0100

    carts: audioManager: implement
    Implement the cart audio manager to simply keep a queue of
    QMediaPlayer instancies.

--- src/carts/audioManager.cpp
+++ src/carts/audioManager.cpp
@@ -1,6 +1,27 @@
 #include "audioManager.h"
 
-AudioManager::AudioManager(QObject *parent) : QObject(parent)
+AudioManager::AudioManager(QObject *parent,
+                           unsigned int noMediaPlayers) : QObject(parent)
 {
+    for (size_t i = 0; i < noMediaPlayers; i++)
+        mediaPlayers_.push(new QMediaPlayer(this));
+}
 
+QMediaPlayer* AudioManager::acquireMediaPlayer(void)
+{
+    if (mediaPlayers_.empty())
+        return NULL;
+
+    QMediaPlayer *ret = mediaPlayers_.front();
+    mediaPlayers_.pop();
+
+    return ret;
+}
+
+void AudioManager::releaseMediaPlayer(QMediaPlayer *player)
+{
+    // Sanitize the obejct before pushing it back into the queue.
+    player->disconnect();
+
+    mediaPlayers_.push(player);
 }
--- src/carts/audioManager.h
+++ src/carts/audioManager.h
@@ -2,16 +2,24 @@
 #define AUDIOMANAGER_H
 
 #include <QObject>
+#include <queue>
+#include <QMediaPlayer>
 
 class AudioManager : public QObject
 {
     Q_OBJECT
 public:
-    explicit AudioManager(QObject *parent = nullptr);
+    AudioManager(QObject *parent = nullptr,
+                 unsigned int noMediaPlayers = 5);
 
+    QMediaPlayer* acquireMediaPlayer(void);
+    void releaseMediaPlayer(QMediaPlayer *player);
 signals:
 
 public slots:
+
+private:
+    std::queue<QMediaPlayer *> mediaPlayers_;
 };
 
 #endif // AUDIOMANAGER_H
