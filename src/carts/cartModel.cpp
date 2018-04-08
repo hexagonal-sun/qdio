0a928fcf64a1ff4de1693d4b760b4b4581d3baf3
Matthew Leach <matthew@mattleach.net>
2018-04-08 11:13:32 +0100

    carts: cartModel: set cart colours when constructing the object
    When constructing cart objects, the the appropriate cart colours.

--- src/carts/cartModel.cpp
+++ src/carts/cartModel.cpp
@@ -56,7 +56,9 @@ CartButton *CartModel::getButton(const enum CartWallType &cartWall,
     if (cart == stationCarts_.end())
         return new CartButton(parent);
 
-    return new CartButton(parent, cart->second.title, "foo", audioMan);
+    return new CartButton(parent, cart->second.title, "foo", audioMan,
+                          cart->second.theme.textColour,
+                          cart->second.theme.bgColour);
 }
 
 bool CartModel::isReady() const
