e11a27e6f45fc216022dae24a88405298ed3c0da
Matthew Leach <matthew@mattleach.net>
2019-01-19 21:50:13 +0000

    model: functions: `get_{station,}_cartwalls_for_show': new
    Add a new set of functions for retrieving cart walls for a particular
    show.

--- model/functions.sql
+++ model/functions.sql
@@ -70,3 +70,60 @@ BEGIN
        WHERE user_show.user_id = uid;
 END;
 $$ LANGUAGE plpgsql;
+
+-- For a give show, find the station's cartwall ids.
+CREATE OR REPLACE FUNCTION get_station_cartwalls_for_show(showid INTEGER)
+RETURNS TABLE (
+    id INTEGER,
+    name TEXT
+)
+AS $$
+DECLARE
+    stationid INTEGER;
+BEGIN
+    SELECT show.owner INTO stationid FROM show WHERE show.id = showid;
+
+    RETURN QUERY SELECT cart_wall.id, cart_wall.name
+         FROM cart_wall
+         WHERE cart_wall.station_owner = stationid;
+END;
+$$ LANGUAGE plpgsql;
+
+-- For a given show, return all the cartwall ids.
+CREATE OR REPLACE FUNCTION get_cartwalls_for_show(showid INTEGER)
+RETURNS TABLE (
+    id INTEGER,
+    name TEXT
+)
+AS $$
+BEGIN
+    RETURN QUERY SELECT cart_wall.id, cart_wall.name
+         FROM cart_wall
+         WHERE cart_wall.show_owner = showid;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_carts_for_cartwall(cartwallid INTEGER)
+RETURNS TABLE (
+    title       text,
+    page        integer,
+    x           integer,
+    y           integer,
+    bg_colour   varchar,
+    text_colour varchar
+)
+AS $$
+BEGIN
+    return QUERY SELECT cart.title,
+                        cart.page,
+                        cart.x,
+                        cart.y,
+                        bgcol.hex AS bg_colour,
+                        textcol.hex AS text_colour
+        FROM cart
+        INNER JOIN cart_theme ON cart.theme = cart_theme.id
+        INNER JOIN colour AS bgcol ON cart_theme.bg_colour = bgcol.id
+        INNER JOIN colour AS textcol ON cart_theme.text_colour = textcol.id
+        WHERE cart.wall = cartwallid;
+END;
+$$ LANGUAGE plpgsql;
