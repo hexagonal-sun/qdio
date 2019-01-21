dd9b1b7abdcd1051043b158536ee0a95d9620aa6
Matthew Leach <matthew@mattleach.net>
2019-01-21 18:39:53 +0000

    model: fixtures: change colours
    Change the colours of the cart buttons to something sensible.

--- model/fixtures.sql
+++ model/fixtures.sql
@@ -22,16 +22,32 @@ insert into cart_wall(show_owner, name) values (1, 'Matts show cart wall'),
                                                (2, 'james show cart wall');
 
 insert into cart_theme(name, bg_colour, text_colour) values
-          ('default', 1, 5),
-          ('Ad', 3, 6),
-          ('Jingle', 4, 6),
-          ('News', 2, 1),
-          ('Bed', 6, 2);
+          ('default', 5, 9),
+          ('Ad', 3, 8),
+          ('Jingle', 2, 8),
+          ('News', 6, 8),
+          ('Bed', 4, 8);
 
 insert into audio_file(name,location,show_owner) values
                       ('Virgin bed', 'virgin.mp3', 1),
                       ('Saw bed', 'saw.m4a', 1);
 
-insert into cart values ('Virgin Bed', 4, 0, 0, 0, 1, 1),
-                        ('Saw Bed', 4, 0, 1, 0, 2, 2);
+insert into audio_file(name,location,station_owner) values                      
+                      ('survival', 'survival.mp3', 1),
+                      ('inside', 'inside.mp3', 1),
+                      ('cheesy 80s', 'cheesy_80s.mp3', 1),
+                      ('breakdown', 'breakdown_bed.mp3', 1),
+                      ('Quiz Bed', 'quiz_bed.mp3', 1),
+                      ('News Bed', 'news_bed.mp3', 1),
+                      ('News intro', 'news_intro.mp3', 1);
+
+insert into cart values ('Virgin Bed',      4, 0, 0, 0, 1, 1),
+                        ('Saw Bed',         4, 0, 1, 0, 2, 2),
+                        ('Survival',        3, 0, 0, 0, 1, 3),
+                        ('Inside',          3, 0, 1, 0, 2, 4),
+                        ('Cheesy 80s',      3, 0, 2, 0, 2, 5),
+                        ('Breakdown & Bed', 3, 0, 3, 0, 2, 6),
+                        ('Quiz Bed',        3, 0, 4, 0, 3, 7),
+                        ('News Bed',        3, 0, 0, 3, 4, 8),
+                        ('News Intro',      3, 0, 1, 3, 4, 9);
 
