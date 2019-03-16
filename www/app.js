2e266af479857152dd8bf7ad7371fcb9e391bef6
Matthew Leach <matthew@mattleach.net>
2019-03-16 19:20:43 +0000

    www: app: export bootstrap static files
    Export the bootstrap static files to the client and import them in the
    layout template.

--- www/app.js
+++ www/app.js
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules',
                                             'jquery', 'dist')));
 
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules',
+                                               'bootstrap', 'dist')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
