d77298917a52aad9c5b4d7ef45b17b37d1151fd6
Matthew Leach <matthew@mattleach.net>
2019-03-16 19:18:48 +0000

    www: app: export jquery static files
    Export the jquery static files to the client and import them in the
    layout template.

--- www/app.js
+++ www/app.js
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/jquery', express.static(path.join(__dirname, 'node_modules',
+                                            'jquery', 'dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
