6405349d6dc5ddb1770ce24d3cbda65684d51ce3
Matthew Leach <matthew@mattleach.net>
2019-03-17 13:00:07 +0000

    www: index: create signin page
    Create a signin page as the index of the site, using the userAuth
    callbacks to do user authentication.

---
+++ www/public/js/userAuth.js
@@ -0,0 +1,88 @@
+function hexStringToByte(str)
+{
+  if (!str) {
+    return new Uint8Array();
+  }
+
+  var a = [];
+  for (var i = 0, len = str.length; i < len; i+=2) {
+    a.push(parseInt(str.substr(i,2),16));
+  }
+
+  return new Uint8Array(a);
+}
+
+function disableControls(state)
+{
+    var inputUsername = $('#inputUsername');
+    var inputPassword = $('#inputPassword');
+    var submitButton = $('#submitButton');
+    var buttonSpinner = $('#buttonSpinner');
+
+    inputUsername.prop('disabled', state);
+    inputPassword.prop('disabled', state);
+    submitButton.prop('disabled', state);
+    buttonSpinner.prop('hidden', !state);
+}
+
+async function userLogin()
+{
+    var inputUsername = $('#inputUsername');
+    var inputPassword = $('#inputPassword');
+    var result;
+
+    disableControls(true);
+
+    try {
+        result = await $.ajax({
+            type: 'POST',
+            url: '/userAuth/findUser',
+            data: JSON.stringify({
+                username: inputUsername.val()
+            }),
+            processData: false,
+            contentType: 'application/json'
+        });
+
+        var saltByteArray = hexStringToByte(result.salt);
+
+        var hash = keccak256.update(saltByteArray)
+            .update(inputPassword.val())
+            .hex()
+
+        result = await $.ajax({
+            type: 'POST',
+            url: '/userAuth/userLogin',
+            data: JSON.stringify({
+                uid: result.user_id,
+                hash: hash
+            }),
+            processData: false,
+            contentType: 'application/json'
+        });
+
+        if (!result)
+        {
+            $('#alertDanger').text('Invalid username or password')
+            $('#alertDanger').fadeIn()
+            $('#inputUsername').focus()
+            disableControls(false);
+            return;
+        }
+
+        window.location.replace('/dashboard/');
+
+    } catch (e) {
+        $('#alertDanger').text('Failed to login: server error')
+        $('#alertDanger').fadeIn()
+        disableControls(false);
+    }
+}
+
+$(document).ready(() => {
+    $('#formSignIn').submit((e) => {
+        e.preventDefault();
+        $('#alertDanger').hide()
+        userLogin();
+    });
+});
--- www/routes/index.js
+++ www/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    if (req.session.uid)
+    {
+        return res.redirect('/dashboard/')
+    }
+
+    res.render('index');
 });
 
 module.exports = router;
