8e43f06dfadb6855e91d41bf48640452fb80dc85
Matthew Leach <matthew@mattleach.net>
2019-03-17 10:54:46 +0000

    www: app: add session support
    Add support for sessions to the app.

--- www/app.js
+++ www/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var expressSession = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -17,6 +18,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: 'foobarbaz',
+    resave: false,
+    saveUninitialized: false
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules',
                                             'jquery', 'dist')));
