a30350de953ad4b07205d2a168171d27af5dfcfe
Matthew Leach <matthew@mattleach.net>
2019-12-06 13:26:58 +0000

    [www/userAuth] add semi-colons to the end of lines
    As suggested by the linter, add semi-colons to the end of lines.

--- www/routes/userAuth.js
+++ www/routes/userAuth.js
@@ -3,51 +3,52 @@ var pg = require('pg');
 var router = express.Router();
 
 router.post('/findUser', async (req, res, next) => {
-    const { Client } = require('pg')
+    const { Client } = require('pg');
     const client = new Client({
         database: 'qdio'
-    })
+    });
 
     try {
-        await client.connect()
+        await client.connect();
 
-        const query = 'SELECT * FROM find_user($1) AS f(user_id integer, salt varchar)'
-        const qRes = await client.query(query, [req.body.username])
+        const query = 'SELECT * FROM find_user($1) AS f(user_id integer, salt varchar)';
+        const qRes = await client.query(query, [req.body.username]);
 
-        res.send(qRes.rows[0])
+        res.send(qRes.rows[0]);
 
         await client.end();
-
     } catch (e) {
-        return res.status(500).send(e)
+        console.log(e);
+        return res.status(500).send(e);
     }
 });
 
 router.post('/userLogin', async (req, res, next) => {
-    const { Client } = require('pg')
+    const { Client } = require('pg');
     const client = new Client({
         database: 'qdio'
-    })
+    });
 
     try {
-        await client.connect()
+        await client.connect();
 
-        const query = 'SELECT user_login($1, $2)'
+        const query = 'SELECT user_login($1, $2)';
+
         const qRes = await client.query(query,
-                                        [req.body.uid, req.body.hash])
+                                        [req.body.uid, req.body.hash]);
 
-        var loginSuccessful = qRes.rows[0].user_login;
+        let loginSuccessful = qRes.rows[0].user_login;
 
         if (loginSuccessful)
             req.session.uid = req.body.uid;
 
-        res.send(loginSuccessful)
+        res.send(loginSuccessful);
 
         await client.end();
 
     } catch (e) {
-        console.log(e)
-        return res.status(500).send()
+        console.log(e);
+        return res.status(500).send();
     }
 });
 
