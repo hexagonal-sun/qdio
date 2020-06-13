6fb4f4a917cdd047490a0541f2a79a2c7c55c412
Matthew Leach <matthew@mattleach.net>
2020-06-13 12:30:53 +0100

    [www/server] remove all front end code
    Remove all views, and routes related to the front end as this has been
    superseded by React.

--- www/server/src/app.ts
+++ www/server/src/app.ts
@@ -5,17 +5,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var userAuthRouter = require('./routes/userAuth');
-var dashboardRouter = require('./routes/dashboard');
 let cartRouter = require('./routes/carts');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,23 +19,9 @@ app.use(expressSession({
     resave: false,
     saveUninitialized: false
 }))
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/jquery', express.static(path.join(__dirname, 'node_modules',
-                                            'jquery', 'dist')));
-
-app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules',
-                                               'bootstrap', 'dist')));
-
-app.use('/sha3', express.static(path.join(__dirname, 'node_modules',
-                                          'js-sha3', 'src')));
-
-app.use('/feather', express.static(path.join(__dirname, 'node_modules',
-                                             'feather-icons', 'dist')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/userAuth', userAuthRouter);
-app.use('/dashboard', dashboardRouter);
 app.use('/carts', cartRouter);
 
 module.exports = app;
