efe41aceaa607143b3b390ff10503752085c8fdc
Matthew Leach <matthew@mattleach.net>
2020-06-28 18:25:43 +0100

    [www/client/src/interfaces/carts] add ICart
    Add the ICart interface.

--- www/client/src/interfaces/carts.ts
+++ www/client/src/interfaces/carts.ts
@@ -1,3 +1,14 @@
-export interface ICartWall {
+export interface ICart
+{
+    title : string,
+    text_colour : string,
+    bg_colour : string,
+    x : number,
+    y : number,
+};
+
+export interface ICartWall
+{
     name : string,
+    id : number,
 };
